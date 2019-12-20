322798666d7f7f5ba13ddd424513cfb060d38fe7
Deepak JOSHI <joshideepak4598@gmail.com>
2019-12-20 20:16:59 +0530

    question 7

---
+++ StudentDB.sql
@@ -0,0 +1,12 @@
+create database studentdb;
+use studentdb;
+
+CREATE TABLE student(
+	rollno INTEGER(10),
+	name VARCHAR(30),
+	hindi INTEGER,
+	eng INTEGER,
+	phy INTEGER,
+	chem INTEGER
+	maths INTEGER
+);

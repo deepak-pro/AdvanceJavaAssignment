1537a08b891767b0cf88c2130c700c38d315005a
Deepak JOSHI <joshideepak4598@gmail.com>
2019-12-20 11:16:31 +0530

    Add program for question 0

---
+++ 0.java
@@ -0,0 +1,28 @@
+import java.util.* ;
+import java.io.* ;
+
+class ArrayListDemo {
+	public static void main(String[] args) {
+		System.out.println("Hello ") ;
+
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(2);
+		list.add(1);
+		list.add(3);
+		list.add(5);
+		list.add(4);
+
+		list.remove(2) ;
+		list.add(4,1) ;
+
+		if(list.contains(5))
+			System.out.println("List contains 5") ;
+
+		System.out.println("Index of 3 is " + index) ;
+
+		Iterator i = list.iterator();
+		while(i.hasNext()){
+			System.out.println(i.next());
+		}
+	}
+}
